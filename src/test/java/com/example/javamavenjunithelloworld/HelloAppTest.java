d07bd99bcb42e8b33fe5b53f9b5316c29a9ed423
amruthaphat <amrutha.bhat@infosys.com>
2016-12-21 17:55:35 +0530

    Trial

--- src/test/java/com/example/javamavenjunithelloworld/HelloAppTest.java
+++ src/test/java/com/example/javamavenjunithelloworld/HelloAppTest.java
@@ -9,13 +9,7 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import static org.mockito.Mockito.*;
 import static org.powermock.api.mockito.PowerMockito.whenNew;
 
-/**
- * Unit test for HelloApp.
- * <p/>
- * A unit test aims to test all code and code paths of a specific class and this is just a trial
- * <p/>
- * This test uses PowerMock and Mockito to mock objects.
- */
+
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({System.class, HelloApp.class})
 public class HelloAppTest {
