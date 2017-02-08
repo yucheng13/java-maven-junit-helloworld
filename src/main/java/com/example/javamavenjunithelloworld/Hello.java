981ee2968fd859157f12abd9a932a4a00530fdf0
amruthapbhat <amrutha.bhat@infosys.com>
2017-02-07 19:58:37 -0800

    Update Hello.java

--- src/main/java/com/example/javamavenjunithelloworld/Hello.java
+++ src/main/java/com/example/javamavenjunithelloworld/Hello.java
@@ -5,14 +5,12 @@ public class Hello {
     static final String HELLO = "Hello!";
     public static final int MAXIMUM_AMOUNT_OF_TIMES = 20;
     private short times = 1;
-
     public void setTimes(int times) {
         if (times < 0 || times > MAXIMUM_AMOUNT_OF_TIMES) {
             throw new IllegalArgumentException("Parameter should be a positive integer no larger than " + MAXIMUM_AMOUNT_OF_TIMES + ".");
         }
-        this.times = (short) times;
+                this.times = (short) times;
     }
-
     public void sayHello(PrintStream printer) {
         for (short i = 0; i < times; i++) {
             printer.println(HELLO);
