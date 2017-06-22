29cf8e0fa21eee0e41f5576e422d49a697327de1
amruthapbhat <amrutha.bhat@infosys.com>
2017-06-22 08:50:01 +0530

    Update HelloTest.java

--- src/test/java/com/example/javamavenjunithelloworld/HelloTest.java
+++ src/test/java/com/example/javamavenjunithelloworld/HelloTest.java
@@ -20,7 +20,7 @@ public class HelloTest {
         Hello hi = new Hello();
         hi.sayHello(stream);
 
-        assertThat(os.toString(), is(equalTo(Hello.HELLO + "\r\n")));
+        assertThat(os.toString(), is(equalTo(Hello.HELLO + "\r")));
     }
 
     @Test
@@ -33,7 +33,7 @@ public class HelloTest {
         hi.sayHello(stream);
 
         // Does it say "Hello!" three times?
-        String goal = Hello.HELLO + "\r\n" + Hello.HELLO + "\r\n" + Hello.HELLO + "\r\n";
+        String goal = Hello.HELLO + "\r" + Hello.HELLO + "\r" + Hello.HELLO + "\r";
         assertThat(os.toString(), is(equalTo(goal)));
     }
 
